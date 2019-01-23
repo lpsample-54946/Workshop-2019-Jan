b2a7bb18c82c6e39b992fa6dd4cc2d382575e02b
lpsample-54946 <psample@cisco.com>
2019-01-23 13:19:17 -0500

    Updated to remove any solution

--- dnainventory.py
+++ dnainventory.py
@@ -1,7 +1,24 @@
 """
+Copyright (c) 2018 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.0 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
+"""
 Import required packages
 We will be using the requests package for HTTP requests and the
 csv package to generate an inventory file
+"""
+"""
+Import required packages
 """
 import requests
 import csv
@@ -9,112 +26,138 @@ import csv
 """
 Set global variables
 """
+# Cisco DNA Center connection variables:
 dna_host = ""
-dna_port = ""
+dna_port = "443"
 dna_username = ""
-dna_password = ""
+dna_password = "!"
 
-baseurl = ""
+# Set the base URL for API calls
+baseurl = "https://{0}:{1}".format(dna_host, dna_port)
 
+# Name of the CSV file for the inventory output
 inventoryfile = "inventory.csv"
-
-"""
-Connect to Cisco DNA Center and get a token
-"""
-# Send a POST request to the proper URL and save the token
-
-"""
-For simplicity, set the headers we'll use
-"""
-# Create a dictionary which contains the Content-Type and x-auth-token
-# header values.
-
-"""
-We don't want to be overwhelmed with data, so let's process
-100 devices at a time.  There's an API call which allows us to
-get a pagination range of devices... we need a start index and
-an end index for each request.  When we hit the end, the response
-should contain an empty list aka []
-"""
-start = 1
-end = 100
-
-"""
-Each program should use the following to test that we're entering
-the actual script:
-"""
 if __name__ == "__main__":
     """
-    Set some initial variables that will be used to collect data
+    Connect to Cisco DNA Center and get a token
     """
-    # Define an empty list for the devices
+    # Insert code here.  Assign the token to a variable named 'dnatoken'
+
+    """
+    For repeatability, set the headers we'll use
+    """
+    headers = {'Content-type': 'application/json',
+               'x-auth-token': dnatoken
+               }
+
+    """
+    We don't want to be overwhelmed with data, so let's process
+    100 devices at a time (we can change this number using the 'step'
+    variable defined below).  There's an API call which allows us to
+    get a pagination range of devices... we need a start index and
+    an end index for each request.  When we hit the end, the response
+    should contain an empty list aka []
+    """
+    step = 100
+    start = 1
+    end = step
+
+    """
+    Initialize an empty list to hold the device information.  Each row
+    in this list will contain information for a single device.
+    """
     devices = list()
+    """
+    The CSV file will contain a header.  The header fields will match the
+    JSON keys for the information we're looking for.  The field names were
+    obtained initially by looking at the output from a Postman call
+    """
+    # Replace the values below with what we expect to see in the JSON response...
+    # csvheader = [hostname, serial, platform, MAC, version]
 
     """
-    What values do we want for the CSV header?
+    Begin iterating over the inventory returned from Cisco DNA center.
     """
-    csvheader = ["col1", "col2", "col3"]
-
-    """
-    Begin the list with the CSV header
-    """
-    devices.append( csvheader)
-
-    """
-    Iterate over a response
-    Unless a specific condition is met, loop.  We want to get a URL and check
-    the response.  Once we're out of devices, the response should be an empty list.
-    At that point, exit the loop.
-    As long as data is returned, we'll extract the key pieces and add a new entry
-    to the list containing the data we want.
-    """
-
     while True:
-        # url = URL
-        r = requests.get( url, headers=headers)
-        """
-        Convert the response to JSON and begin checking the data
-        """
-        # Convert and parse...
+        # Find the correct URL and modify the below variable accordingly.
+        # url = "{0}/".format(baseurl, start, end)
+
+        # Task: send the correct request to the URL.  Convert to JSON, and show the response.
+        # r = requests.
 
         """
-        Test for an empty list.  If it's not empty, let's extract our stuff
-        and append to the devices list
+        Remember, if the response is an empty list, we're at the end and can break.
+        If the response isn't empty, do something!
         """
         if r != []:
-            """
-            Iterate through the response and get interesting fields
-            """
-            row = [ device['field1'], device['field2'], device['...'] ]
-            # Append this row to our inventory
-            devices.append( row)
-            """
-            Increment our start page and end page by some value...
-            """
-            # start ... add 100
-            # end ... add 100
+            for device in r:
+                """
+                Initialize an empty list.  We will begin populating rows for the
+                'devices' list initialized earlier.
+                The idea here is that we can change which values we want to put in
+                the CSV file based on the 'csvheader' fields.
+                To accomplish this, we will EXTEND the 'fields' list with each value
+                found from the 'csvheaders' variable.  Once we're iterated over all
+                the headers, APPEND the 'devices' master list with the 'fields' list
+                created here.  That will add a new row to our CSV file.
+                """
+                fields = list()
+
+                # Task: iterate over the values of 'csvheader' - if a value is found for each
+                # of those keys, EXTEND the 'fields' list.
+                #
+                # for ...
+                    # Dynamically get values for each CSV header field
+                    # Task: Replace '...var...' with a valid value:
+
+                    # fields.extend( ...var...)
+
+                # Task: Append the 'fields' list to the 'devices' list
+                # ...append...
 
             """
-            Update the URL we'll access with the new start and end values.
-            Nothing left after that, so the loop will iterate
+            We've iterated over the first (step) devices in the inventory.  Now
+            increment the start and end values by (step) in preparation of the
+            next iteration.
             """
-            # url = ...
-    else:
-        """
-        The result is an empty set, so we'll end the loop here
-        """
-        break
+            # Task: increment the start and end variables by the value of 'step':
+            # start += ...var...
+            # end += ...var...
+        else:
+            """
+            Empty list encountered in the HTTP response.  Exit the loop and create
+            a CSV file
+            """
+            break
+    """
+    Open a new file handle for writing named 'invfile'
+    We will write the CSV header first, followed by each row of the 'devices' list
+    which contains the final inventory:
+    """
 
-    """
-    We're outside the loop now and should have a full inventory.  Let's write it to a CSV!
-    """
-    # Open the CSV for writing.
-    with open( inventoryfile, 'w') as invfile:
-        # Instantiate a 'csv writer' instance, output to the opened file
-        wr = csv.writer( invfile)
+    with open(inventoryfile, 'w') as invfile:
+        # Instantiate a new csv 'writer' to the destination file 'invfile'
+        wr = csv.writer(invfile)
+        wr.writerow(csvheader)
+
         """
-        Iterate over each row in the 'devices' variable.  This should dump the contents to the CSV file
+        Iterate over each row in the 'devices' list, writing a new line for
+        each row.
         """
-        # Begin for loop
-            # Write the inventory to the file
+        # Task: iterate over the 'devices' variable and put a new row in the CSV:
+        # for ...
+            # Write a row to the CSV...
+
+
+    # Task: Now that we've seen to open a file for writing, open the file for
+    # reading and print each line of the CSV.
+    # Hint: argument 'w' was used in the open statement above to open the file
+    # for WRITING.  Argument 'r' will be needed to open for READING.
+
+    # 1. Open the file for reading.
+    # ...
+        # 2. Iterate over each line of the CSV and print the result.
+        #    HINT: to remove any extra new lines, use the 'rstrip' method
+        #    to remove the newline, represented as '\n'
+
 
